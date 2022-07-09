af739e28d116eb6b8562b8824a462914a9edc6cb
lucianorocha <lkr.junior@outlook.com>
2022-07-09 17:23:36 -0300

    AddDbContext

---
+++ NetEFCore.Core/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NetEFCore.Core.Models;
+
+namespace NetEFCore.Core.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        { }
+
+        public DbSet<PessoaFisica> PessoaFisicas { get; set; }
+        public DbSet<PessoaJuridica> PessoaJuridicas { get; set; }
+
+    }
+}
