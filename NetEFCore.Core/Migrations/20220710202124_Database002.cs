61e353cab0df7e8093dc61a70f56c90ff35f7ef1
lucianorocha <lkr.junior@outlook.com>
2022-07-10 17:22:53 -0300

    Add new column

---
+++ NetEFCore.Core/Migrations/20220710202124_Database002.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetEFCore.Core.Migrations
+{
+    public partial class Database002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisterDate",
+                table: "PessoaJuridicas",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegisterDate",
+                table: "PessoaJuridicas");
+        }
+    }
+}
