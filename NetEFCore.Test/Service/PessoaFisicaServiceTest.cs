c660b8df77abfc89b5be375a77e3f2d9c91b0bf6
lucianorocha <lkr.junior@outlook.com>
2022-07-12 20:51:56 -0300

    Initial Unit test to service Pessoa Fisica

---
+++ NetEFCore.Test/Service/PessoaFisicaServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NetEFCore.Core.Interfaces;
+using NetEFCore.Core.Models;
+using NetEFCore.Core.Service;
+
+namespace NetEFCore.Test.Service
+{
+    public class PessoaFisicaServiceTest
+    {
+        [Fact]
+        public async Task ShouldCreatePessoaFisicaWhenPayloadIsValid()
+        {
+            var entity = new PessoaFisica()
+            {
+                Name = "Name",
+                Cpf = "1",
+                BirthDate = new DateTime(1980, 1, 1)
+            };
+            var expectedEntity = default(PessoaFisica);
+
+            var mockRepository = new Mock<IRepositoryBase<PessoaFisica>>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+
+            mockRepository.Setup(x => x.InsertAsync(It.IsAny<PessoaFisica>())).Returns((PessoaFisica entityToSave) =>
+            {
+                expectedEntity = entityToSave;
+
+                return Task.CompletedTask;
+            });
+
+            mockUnitOfWork.Setup(x => x.PessoaFisicaRepository).Returns(mockRepository.Object);
+
+            var service = new PessoaFisicaService(mockUnitOfWork.Object);
+
+            var result = await service.Insert(entity);
+
+            Assert.False(result.HasError);
+            Assert.Equal(StatusCodes.Status200OK, result.HttpStatusCode);
+            Assert.Equal(expectedEntity, entity);
+        }
+    }
+}
+
